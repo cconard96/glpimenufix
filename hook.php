b8caed7562cc3949f0ab4194ecb9025ae35e8b10
cconard96 <cconard96@gmail.com>
2020-08-27 17:25:31 -0400

    Add copyright headers

--- hook.php
+++ hook.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ -------------------------------------------------------------------------
+ GLPI Menu Fix Plugin
+ Copyright (C) 2020 by Curtis Conard
+ https://github.com/cconard96/glpimenufix
+ -------------------------------------------------------------------------
+ LICENSE
+ This file is part of GLPI Menu Fix Plugin.
+ GLPI Menu Fix Plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ GLPI Menu Fix Plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with GLPI Menu Fix Plugin. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
 
 function plugin_menufix_install()
 {
